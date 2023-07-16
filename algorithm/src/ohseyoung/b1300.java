5bdc6f937aaf437ec4b8272bf199994ea0d5020c
Seyoung Oh <osy9536@naver.com>
2023-07-16 14:55:23 +0900

    1300번 : k번째 수

--- algorithm/src/ohseyoung/b1300.java
+++ algorithm/src/ohseyoung/b1300.java
@@ -1,25 +1,23 @@
 package algorithm.src.ohseyoung;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 // k번째 수
 public class b1300 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        long ans = 0;
+        int ans = 0;
 //        1 2 3 4;
 //        2 4 6 8;
 //        3 6 9 12;
 //        4 8 12 16;
-        long start = 1;
-        long end = k;
+        int start = 1, end = k, middle;
 
         while (start <= end) {
-            long middle = (start / end) / 2;
+            middle = (start + end) / 2;
             long cnt = 0;
             for (int i = 1; i <= n; i++) {
                 cnt += Math.min(middle / i, n);
@@ -31,6 +29,9 @@ public class b1300 {
                 end = middle - 1;
             }
         }
-        System.out.println(ans);
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
