931f8a5fc0c0a0bcf1eed28374a1b5de57643687
freezinghands <75113687+freezinghands@users.noreply.github.com>
2023-07-17 23:22:22 +0900

    이재윤_(13300)

---
+++ algorithm/src/jaeyun/b13300.java
@@ -0,0 +1,29 @@
+package algorithm.src.jaeyun;
+
+import java.util.Scanner;
+
+public class b13300 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] male = new int[7];
+        int[] female = new int[7];
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 1) {
+                male[b] += 1;
+            }
+            else {
+                female[b] += 1;
+            }
+        }
+        for (int i=1; i<7; i++) {
+            sum += (male[i] + K - 1) / K;
+            sum += (female[i] + K - 1) / K;
+        }
+        System.out.println(sum);
+    }
+}
