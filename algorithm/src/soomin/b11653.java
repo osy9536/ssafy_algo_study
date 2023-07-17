40b7f1bf5ccd49ed0051ec58cfa566b673b11cc2
dalcheonroadhead <wjsaos2081@gmail.com>
2023-07-17 21:34:21 +0900

    [전수민]_11653

--- algorithm/src/soomin/b11653.java
+++ algorithm/src/soomin/b11653.java
@@ -8,6 +8,7 @@ public class b11653 {
 
     /*https://www.acmicpc.net/problem/11653*/
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
