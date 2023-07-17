06aa6865b3e6dee21cd817f3d7d33a06f9c32e9f
dalcheonroadhead <wjsaos2081@gmail.com>
2023-07-17 21:31:41 +0900

    [전수민]_11653

---
+++ algorithm/src/soomin/b11653.java
@@ -0,0 +1,31 @@
+package algorithm.src.soomin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b11653 {
+
+    /*https://www.acmicpc.net/problem/11653*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int i =2;
+
+        int primeFactor = 1;
+
+        while(primeFactor != N){
+            if(N%i == 0){
+                System.out.println(i);
+                N /= i;
+            }else{
+                i++;
+            }
+        }
+
+
+    }
+}
--- algorithm/src/soomin/test.java
+++
@@ -1,7 +0,0 @@
-package algorithm.src.soomin;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("-------------");
-    }
-}
