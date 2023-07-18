5463931fb7ab8bec187fef86c0d9475adeb61f00
dalcheonroadhead <wjsaos2081@gmail.com>
2023-07-18 22:27:46 +0900

    [전수민]_1085

--- algorithm/src/soomin/b1085.java
+++ algorithm/src/soomin/b1085.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class b1085 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
