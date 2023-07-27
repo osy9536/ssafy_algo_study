c78719199f36c743d56719c787e55710f2010bef
GEISHAz <allmin9702@naver.com>
2023-07-27 22:01:59 +0900

    요세푸스 문제 : 11866번

---
+++ algorithm/src/minho/b11866.java
@@ -0,0 +1,33 @@
+package algorithm.src.minho;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class b11866{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1;i<=a;i++)
+            q.add(i);         //입력
+        while(!q.isEmpty()) {
+            for(int i=1;i<b;i++) {
+                int p=q.poll();
+                q.add(p);
+            }
+            list.add(q.poll());
+        }
+        System.out.print("<");
+        for(int i=0;i<list.size()-1;i++)
+            System.out.print(list.get(i)+", ");
+        System.out.print(list.get(list.size()-1)+">");
+    }
+}
