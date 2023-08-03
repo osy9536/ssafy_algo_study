681778a5cfd22b810de2cb8ea6c5e9382d63e5e9
JunSeo Lee <wnstj7866@naver.com>
2023-08-03 22:24:47 +0900

    b11660

---
+++ algorithm/src/junseo/b11660_v2.java
@@ -0,0 +1,46 @@
+package algorithm.src.junseo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b11660_v2 {
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][] arr = new int [N+1][N+1];
+        int [][] prefix = new int [N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        //////////////////////////////////////////////////////////////////////////////
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j];
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////////////
+
+        StringBuilder  sb = new StringBuilder();
+        for(int i = 1;i<=M;i++) {
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            sum = prefix[x2][y2] - prefix[x2][y1-1] - prefix[x1-1][y2] + prefix[x1-1][y1-1];
+            sb.append(sum+"\n");
+            //System.out.println(sum);
+        }
+        System.out.println(sb);
+    }
+}
+
