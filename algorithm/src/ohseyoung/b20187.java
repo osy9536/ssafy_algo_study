b587dafb1ccdfcee8776c4f606edbdf964bb0865
Seyoung Oh <osy9536@naver.com>
2023-08-15 23:07:40 +0900

    종이접기 : 20187

--- algorithm/src/ohseyoung/b20187.java
+++ algorithm/src/ohseyoung/b20187.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 종이접기
+// gold 3
 public class b20187 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
