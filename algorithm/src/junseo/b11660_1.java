a78b6c6fab66b643baf2ef26f7442a1ee84477ac
quasiwest <108654626+quasiwest@users.noreply.github.com>
2023-08-23 09:24:45 +0900

    구간 합 구하기 5_1 : 11660

--- algorithm/src/junseo/b11660_1.java
+++ algorithm/src/junseo/b11660_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class b11660_v1 {
+public class b11660_1 {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
