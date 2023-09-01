ed3deedf649b334fc21c8aab1695e5c408c0cac8
quasiwest <108654626+quasiwest@users.noreply.github.com>
2023-09-01 19:18:36 +0900

    부분합 : 1806

---
+++ algorithm/src/junseo/b1806.java
@@ -0,0 +1,46 @@
+package algorithm.src.junseo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = 0;
+        int currentSum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while (true) {
+            if (currentSum >= s) {
+                minLength = Math.min(minLength, right - left);
+                currentSum -= arr[left];
+                left++;
+            } else if (right == n) {
+                break;
+            } else {
+                currentSum += arr[right];
+                right++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(minLength);
+        }
+    }
+}
+
