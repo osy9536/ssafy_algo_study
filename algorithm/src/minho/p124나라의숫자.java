8d50a841675efbdf2e21b4e86d55eb0ecef714f2
GEISHAz <allmin9702@naver.com>
2023-10-13 19:58:00 +0900

    124나라의 숫자

---
+++ algorithm/src/minho/p124나라의숫자.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        Stack<Integer> stack = new Stack <>();
+        String answer = "";
+        while(n != 0) {
+            int temp = n % 3;
+            
+            if(temp == 0) {
+                temp = 4;
+                n = n / 3 - 1;
+            } else {
+                n /= 3;
+            }
+            stack.push(temp);
+        }
+        while(!stack.isEmpty()){
+            answer+=stack.pop();
+        }
+        return answer;
+    }
+}
