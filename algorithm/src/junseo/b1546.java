b6648df55f6d6333da7984a8f2003852bcc50acc
이준서 <108654626+quasiwest@users.noreply.github.com>
2023-10-18 19:22:05 +0900

    평균 : 1546

---
+++ algorithm/src/junseo/b1546.java
@@ -0,0 +1,30 @@
+package algorithm.src.junseo;
+
+package doIt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1546 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    int [] A = new int[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i <N; i++) {
+      A[i] = Integer.parseInt(st.nextToken());
+    }
+    int max = 0;
+    int sum = 0;
+    for (int i = 0; i <N; i++) {
+      sum += A[i];
+      max = Math.max(max,A[i]);
+    }
+    System.out.println((double)sum * 100 / max / N);
+  }
+}
+
+
