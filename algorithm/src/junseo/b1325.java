5426c61597a159591bfc273353ee36328f50e400
이준서 <108654626+quasiwest@users.noreply.github.com>
2023-10-26 17:31:03 +0900

    효율적인 해킹 : 1325

--- algorithm/src/junseo/b1325.java
+++ algorithm/src/junseo/b1325.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
