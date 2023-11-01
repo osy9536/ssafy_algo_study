d7733c0dc6aa4d2d67cc0cce487b46a25b170647
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-11-01 17:33:54 +0900

    이분 그래프 : 1707

---
+++ algorithm/src/ohseyoung/b1707.java
@@ -0,0 +1,76 @@
+package algorithm.src.ohseyoung;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 이분 그래프
+// gold 4
+public class b1707 {
+	static int v, e;
+	static ArrayList<Integer>[] al;
+	static int visit[];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stz = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(stz.nextToken());
+
+		for(int tc = 0; tc < t; tc++) {
+			stz = new StringTokenizer(br.readLine());
+			v = Integer.parseInt(stz.nextToken());
+			e = Integer.parseInt(stz.nextToken());
+			visit = new int[v+1];
+			al = new ArrayList[v+1];
+
+			for(int i = 0; i <= v; i++)
+				al[i] = new ArrayList<Integer>();
+
+			for(int i = 0; i < e; i++) {
+				stz = new StringTokenizer(br.readLine());
+				int p1 = Integer.parseInt(stz.nextToken());
+				int p2 = Integer.parseInt(stz.nextToken());
+
+				al[p1].add(p2);
+				al[p2].add(p1);
+			}
+			grouping();
+		}
+	}
+
+	public static void grouping() {
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		for(int i = 1; i <= v; i++) {
+			if(visit[i] == 0) {
+				q.add(i);
+				visit[i] = 1;
+			}
+
+			while(!q.isEmpty()) {
+				int now = q.poll();
+
+				for(int j = 0; j < al[now].size(); j++) {
+					if(visit[al[now].get(j)] == 0) {
+						q.add(al[now].get(j));
+					}
+					
+					if(visit[al[now].get(j)] == visit[now]) {
+						System.out.println("NO");
+						return;
+					}
+
+					if(visit[now] == 1 && visit[al[now].get(j)] == 0)
+						visit[al[now].get(j)] = 2;
+					else if(visit[now] == 2 && visit[al[now].get(j)] == 0)
+						visit[al[now].get(j)] = 1;
+				}
+			}
+		}
+
+		System.out.println("YES");
+	}
+}
