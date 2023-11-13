be575a5534c90a749f0cd34d53adbc5642b0150d
이준서 <108654626+quasiwest@users.noreply.github.com>
2023-11-13 17:44:21 +0900

    제곱 ㄴㄴ수 : 1016

--- algorithm/src/junseo/b1016.java
+++ algorithm/src/junseo/b1016.java
@@ -1,10 +1,14 @@
 package algorithm.src.junseo;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/*
+ * 참조  https://loosie.tistory.com/720 
+ */
+public class b1016_제곱ㄴㄴ수 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +23,14 @@ public class Main {
 		boolean [] arr = new boolean[arrLength];
 		
 		for (long i = 2; i <= sqrt; i++) {
-			
+			/*
+			 * 제곱수로 나누어지는 시작 인덱스는 
+			 * mn보다 크고 제곱수로 나누어져야 함 
+			 * i*i = 4, mn = 20 일 때, 시작인덱스는 20
+			 * i*i = 9, mn = 20 일 때, 시작인덱스는 27
+			 * 9*3인 27부터 시작해야하는데 이 때 3을 구하기 위해 
+			 * mn을 9로 나누고 +1을 해줌 
+			 */
 			long pow = i*i;
 			long start_idx = mn/pow;
 			if(mn%pow != 0) start_idx++;
