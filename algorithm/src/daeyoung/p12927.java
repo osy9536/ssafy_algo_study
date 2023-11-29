c31be657c65fc6e32779cdcac8598cd6d6e75146
김대머리영감탱 <74356213+kdozlo@users.noreply.github.com>
2023-11-29 21:42:23 +0900

    1129_김대영_p12927

---
+++ algorithm/src/daeyoung/p12927.java
@@ -0,0 +1,41 @@
+package algorithm.src.daeyoung;
+
+import java.util.*;
+
+/**
+ * 프로그래머스 - 야근 지수
+ * Lv 3
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12927
+ */
+public class 야근지수 {
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[] works = {4, 3, 3};
+
+        long answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> (o2 - o1));
+
+        for(int i : works)
+            pq.add(i);
+
+        while(n != 0) {
+            int cur = pq.poll();
+
+            if(cur == 0)
+                break;
+
+            cur -= 1;
+            n -= 1;
+            pq.add(cur);
+        }
+
+        while(!pq.isEmpty()) {
+            int cur = pq.poll();
+            answer += cur * cur;
+        }
+
+        System.out.println(answer);
+    }
+}
