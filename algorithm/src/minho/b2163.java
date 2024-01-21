2f7bc772d6909ab24894c8121cdf2d5e16fe1216
GEISHAz <allmin9702@naver.com>
2024-01-21 21:50:05 +0900

    초콜릿자르기: b2163

---
+++ algorithm/src/minho/b2163.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        count = m * n - 1;
+        System.out.println(count);
+
+    }
+
+}
