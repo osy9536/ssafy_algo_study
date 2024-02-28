db067d9eaa8b588440f3c47608c5f04517fd4a0c
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2024-02-28 09:40:03 +0900

    집합의 표현 : 1717

---
+++ algorithm/src/ohseyoung/b1717.java
@@ -0,0 +1,72 @@
+package algorithm.src.ohseyoung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 집합의 표현
+// gold 5
+public class b1717 {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (x == 0) {
+                union(a, b);
+            } else {
+                if (find(a) == find(b)) {
+                    sb.append("YES").append("\n");
+                } else {
+                    sb.append("NO").append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+
+        return parent[a] = find(parent[a]);
+    }
+
+    static boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb) {
+            return false;
+        }
+
+        if (pa < pb) {
+            parent[pb] = pa;
+        }
+
+        if (pa > pb) {
+            parent[pa] = pb;
+        }
+
+        return true;
+    }
+}
