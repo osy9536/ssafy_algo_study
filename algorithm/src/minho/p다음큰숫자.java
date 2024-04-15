1ef083b121083641f932b3d5ad581cc7481a5014
jungminho <allmin9702@naver.com>
2024-04-15 09:48:52 +0900

    정민호_0415_0421

---
+++ algorithm/src/minho/p다음큰숫자.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean find = false;
+        answer = n;
+        int count = 0,anscnt;
+        count = Integer
+                .toBinaryString(n)
+                .replace("0","")
+                .length();
+        while(true){
+            if(find) break;
+            answer += 1;
+            anscnt = Integer
+                    .toBinaryString(answer)
+                    .replace("0","")
+                    .length();
+            if(count==anscnt){
+                find = true;
+            }
+
+        }
+
+        return answer;
+    }
+}
